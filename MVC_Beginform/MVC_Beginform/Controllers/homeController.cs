4cd9925c05508ebd896fb0aaf03103f26fc6d7fe
pooja <puja.chikhalikar26@gmail.com>
2020-08-24 15:29:48 +0530

    m1

---
+++ MVC_Beginform/MVC_Beginform/Controllers/homeController.cs
@@ -0,0 +1,25 @@
+﻿using MVC_Beginform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC_Beginform.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User_Model um)
+        {
+            return View(um);
+        }
+
+    }
+}
