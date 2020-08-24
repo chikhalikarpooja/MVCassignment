51c9fb66cf6265ab8fa61cf7e593db5bad0f34e3
pooja <puja.chikhalikar26@gmail.com>
2020-08-24 15:59:39 +0530

    MVC Program

---
+++ MVC_EntityFramework_Project/MVC_EntityFramework_Project/Models/Student_Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MVC_EntityFramework_Project.Models
+{
+    public class Student_Context : DbContext
+    {
+        public Student_Context() : base("StudentDatabase")
+        {
+        }
+        public DbSet<student> Students{ get; set; }
+
+    }
+}
