9ec2ea0fbea3a60cc53e3105c7f1a0aea06a971d
pooja <puja.chikhalikar26@gmail.com>
2020-09-16 00:00:00 +0530

    inheritance programs

---
+++ InhertitanceAssignment_practice/InhertitanceAssignment_practice/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InhertitanceAssignment_practice
+{
+    class Member
+    {
+      public  string name, age, phonenumber, address, salary;
+       
+       
+        public void input()
+        {
+            Console.WriteLine("Enter the name of employee:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the age of employee:");
+            age = Console.ReadLine();
+            Console.WriteLine("Enter the phonenumber of employee");
+            phonenumber = Console.ReadLine();
+            Console.WriteLine("Enter the address:");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter the salary of employee");
+            salary = Console.ReadLine();
+        }
+        
+     
+        public void displayemployee()
+        {
+            Console.WriteLine("Employee name is:" + name);
+            Console.WriteLine("Employee age is:" + age);
+            Console.WriteLine("Employee phonenumber is:" + phonenumber);
+            Console.WriteLine("Employee address is:" + address);
+           
+        }
+        public void printsalary()
+        {
+            Console.WriteLine("Total salary is:" + salary);
+        }
+    }
+    class Employee : Member
+    {
+        string specialization ;
+      public  void display()
+        {
+            Console.WriteLine("Enter the specilization of employee");
+            specialization = Console.ReadLine();
+            Console.WriteLine("Employee Spcilization is:" + specialization);
+        }
+
+    }
+    class Manager : Member
+    {
+        string department;
+       public void display1()
+        {
+            Console.WriteLine("Enter the department of employee");
+            department = Console.ReadLine();
+            Console.WriteLine("Employee department is:" +department);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Member m = new Member();
+            m.input();
+            m.displayemployee();
+            m.printsalary();
+            Employee emp = new Employee();
+            emp.display();
+            Manager m1 = new Manager();
+            m1.display1();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice_Functions1/Practice_Functions1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Functions1
+{
+    class Employee
+    {
+        public double amount = 1000;
+ public double Withdraw(int x)
+        {
+            double res = amount-x ;
+            return res;
+        }
+
+        public double Deposit (int y)
+        {
+            double res1 = amount + y;
+            return res1;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int val1;
+            Console.WriteLine("Enter the amount");
+            val1 = Convert.ToInt32(Console.ReadLine());
+            Employee emp = new Employee();
+           
+           
+            double result = emp.Withdraw(val1);
+            Console.WriteLine("Amount after withdraw :"+result);
+            double result1 = emp.Deposit(val1);
+            Console.WriteLine("Amount after deposit:"+result1);
+            Console.ReadKey();
+        }
+    }
+}
